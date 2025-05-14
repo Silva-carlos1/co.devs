355ca86c4fd95685af1a295afee6cc88e3af55b2
Carlos <silva.carloseduardo.ds@gmail.com>
2025-05-14 20:52:00 -0300

    feat(auth, users): implement user registration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Body, Controller, Post, Res, HttpStatus } from '@nestjs/common';
+import { Response } from 'express';
+import { UsersService } from 'src/users/users.service';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly usersService: UsersService,
+  ) {}
+
+  @Post('register')
+  async registerUser(
+    @Body() createUserDto: CreateUserDto,
+    @Res() res: Response,
+  ) {
+    await this.usersService.create(createUserDto);
+
+    return res.status(HttpStatus.NO_CONTENT).send();
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/libs/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { sanitizeString } from 'src/common/utils/sanitize-username.util';
+import { hash } from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const userName = sanitizeString(createUserDto.username);
+
+    const user = await this.prismaService.user.findFirst({
+      where: {
+        OR: [{ email: createUserDto.email }, { username: userName }],
+      },
+    });
+
+    if (user) throw new ConflictException('email or username duplicated');
+
+    await this.prismaService.user.create({
+      data: {
+        username: userName,
+        email: createUserDto.email,
+        name: createUserDto.name,
+        password: await hash(createUserDto.password, 10),
+        created_at: new Date(),
+      },
+    });
+
+    return;
+  }
+}
